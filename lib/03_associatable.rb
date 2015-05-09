ea503095d5dc4c3efe67addfc5046172d79b7101
ougarcia <oz.ulysses@gmail.com>
2015-05-08 17:23:34 -0700

    Almost finish has_one_through

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -47,7 +47,7 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    options = BelongsToOptions.new(name, options)
+    assoc_options[name] = options = BelongsToOptions.new(name, options)
     define_method(name) do
       foreign_key_value = send(options.foreign_key)
       model_class = options.model_class
@@ -63,7 +63,7 @@ module Associatable
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
   end
 end
 
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -5,6 +5,27 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    belongs_to(through_name)
+    self_options = assoc_options[name]
+    through_options = assoc_options[through_name]
+    #very close to finishing
+    define_method(name) do
+      source_options = through_options.model_class.assoc_options[source_name]
+      source_table = source_options.table_name
+      through_table = through_options.table_name
+      x = DBConnection.execute(<<-SQL)
+        SELECT
+        #{source_table}.*
+        FROM
+        #{source_table}
+        INNER JOIN
+        #{through_table}
+        ON #{source_table}.id = #{through_table}.#{source_options.foreign_key}
+        INNER JOIN
+        #{self.class.table_name} ON #{self.class.table_name}.id = #{through_table}.#{self_options.foreign_key};
+      SQL
+
+      x.first
+    end
   end
 end
